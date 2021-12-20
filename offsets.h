dda50ca6ddca900c0e46e7b61254cc39163d94f5
maikel233 <maikel233@live.nl>
2021-12-20 23:44:52 +0100

    Update offsets.h
    Updated a few things

--- offsets.h
+++ offsets.h
@@ -4,7 +4,7 @@
 namespace Offsets
 {
 	////////////////////////
-	//     2.5.1.39640 //
+	//     2.5.2.41446   //
 	////////////////////////
 
 	// base address
@@ -13,28 +13,28 @@ namespace Offsets
 	// framescript
 	static inline uintptr_t FrameScriptExecute = 0x00;
 	static inline uintptr_t FrameScriptGetText = 0x00;	// 
-	static inline uintptr_t FrameScriptRegister = 0x806460;	// 
-	static inline uintptr_t FrameScript_RegisterFunctionNamespaceWithCount = 0x00;
+	static inline uintptr_t FrameScriptRegister = 0x08C7A70;	// 
+	static inline uintptr_t FrameScript_RegisterFunctionNamespaceWithCount = 0x8C79C0;
 
 	// Pointers
-	static inline uintptr_t InGame = Base + 0x2EA55B0;// Script_IsPlayerInWorld
+	static inline uintptr_t IsPlayerInWorld = Base + 0x30C2F24;// Script_IsPlayerInWorld
 	static inline uintptr_t LoadingScreen = Base + 0x00; // good //Reversed From Script_CanLogIn to CGlueMgr__CanLogIn
 	static inline uintptr_t InWorld = Base + 0x00;
-	static inline uintptr_t CGGameUI_s_inWorld = Base + 0x2EA55B0; //   NotInitialized = 0, LoadingScreen1 = 3, LoadingScreen2 = 2, InGame = 4
+	static inline uintptr_t CGGameUI_s_inWorld = Base + 0x30C2F24; //   NotInitialized = 0, LoadingScreen1 = 3, LoadingScreen2 = 2, InGame = 4
 
 	// object manager
-	static inline uintptr_t ClntObjMgrEnumVisibleObjectsPtr = Base + 0x129D9C0;
-	static inline uintptr_t ClntObjMgrGetMapId = Base + 0x12A1760;
-	static inline uintptr_t ClntObjMgrIsValid = Base + 0x12A1ED0;
+	static inline uintptr_t ClntObjMgrEnumVisibleObjectsPtr = Base + 0x13E3EB0;
+	static inline uintptr_t ClntObjMgrGetMapId = Base + 0x13E8680;
+	static inline uintptr_t ClntObjMgrIsValid = Base + 0x13E8DF0;
 
 	//CTM
 	static inline uintptr_t ClickToMove = 0x00; //CGGameUI::HandleTerrainClick
 
 	static inline uintptr_t FaceTo = 0x000;
 	// pointers
-	static inline uintptr_t InvalidPtrCheckMin = Base + 0x2C2CF30;
-	static inline uintptr_t InvalidPtrCheckMax = Base + 0x2C2CF38;
-	static inline uintptr_t HardwareEventPtr = Base + 0x2B9B750;
+	static inline uintptr_t InvalidPtrCheckMin = Base + 0x2E416C0;
+	static inline uintptr_t InvalidPtrCheckMax = Base + 0x2E416C8;
+	static inline uintptr_t HardwareEventPtr = Base + 0x2E1AE78;
 	static inline uintptr_t CanPerformAction = 0x00;
 
 
@@ -47,23 +47,22 @@ namespace Offsets
 	static inline uint16_t Owner = 0x534;
 
 	//cast  
-	static inline uintptr_t Spell_C_GetMinMaxRange = Base + 0xEFD590;
-	static inline uintptr_t Spell_C_GetSpellCoolDown = Base + 0xF00060;
-	static inline uintptr_t castSpell = Base + 0xEF7790; // From Spell_C_CastSpell not the function itself.
-	static inline uintptr_t isSpellKnown = Base + 0x14FE6E0;
-	static inline uintptr_t findSlotBySpellId = Base + 0x1509BB0;
+	static inline uintptr_t Spell_C_GetMinMaxRange = Base + 0x103F080;
+	static inline uintptr_t Spell_C_GetSpellCoolDown = Base + 0x1041B60;
+	static inline uintptr_t castSpell = Base + 0x10395D0; // From Spell_C_CastSpell not the function itself.
+	static inline uintptr_t isSpellKnown = Base + 0x1679300; // Script_IsSpellKnow
+	static inline uintptr_t findSlotBySpellId = Base + 0x16874F0;
 
 	//Globals
 	static inline uintptr_t GetPlayerName = Base + 0x264270;
 		
-	static inline uintptr_t CorpseMapID = Base + 0x00;/
-	static inline uintptr_t Corpsex = Base + 0x2A9B080;  // Credits Razzue from Ownedcore | NOT TESTED!
+	static inline uintptr_t CorpseMapID = Base + 0x00;
+	static inline uintptr_t Corpsex = Base + 0x2CAEAB0;  // Script_RetrieveCorpse -> First xmmword xref -> sub -> Under cintobjmgr first dword.  
 	static inline uintptr_t Corpsey = Corpsex + 0x4;
 	static inline uintptr_t Corpsez = Corpsex + 0x8;
 
 	//Camera WorldFrame::GetActiveCamera
-	static inline  uintptr_t CameraMgr = Base + 0x2F89740;
-	static inline  uintptr_t CameraPtr = 0x38D8; // wowclassic 0x3330;
-
+	static inline  uintptr_t CameraMgr = Base + 0x31A4CA8;
+	static inline  uintptr_t CameraPtr = 0x38E0; // Shifted 8 bytes since 38631
 };
 
