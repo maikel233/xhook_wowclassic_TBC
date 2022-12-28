202ebd0e406a5e736ff007d9f8f1b7610f0960ad
maikel233 <maikel233@live.nl>
2022-12-28 19:53:57 +0100

    Update GInterface.cpp

--- GInterface.cpp
+++ GInterface.cpp
@@ -1,5 +1,28 @@
 #include "GInterface.h"
 
+void Empty() { 
+}
+
+
+LONG NTAPI GInterface::VectoredHandler(EXCEPTION_POINTERS* ExceptionInfo)
+{
+	if (ExceptionInfo->ExceptionRecord->ExceptionCode != 0x80000003 && ExceptionInfo->ExceptionRecord->ExceptionCode != 0xC0000094)
+	{
+		MessageBoxA(nullptr, "Fatal Exception!", to_string(ExceptionInfo->ExceptionRecord->ExceptionCode).c_str(), MB_OK);
+		EXCEPTION_CONTINUE_SEARCH;
+	}
+	else
+	{
+		if (ExceptionInfo->ExceptionRecord->ExceptionCode == 0xC0000094)
+		{
+		//	ExceptionInfo->ContextRecord->Rip = (uint64_t)LuaCall;
+			ExceptionInfo->ContextRecord->Rip = (uintptr_t)Empty;
+			return EXCEPTION_CONTINUE_EXECUTION;
+		}
+	}
+
+	return EXCEPTION_CONTINUE_SEARCH;
+}
 
 void GInterface::WaitForReload()
 {
@@ -24,8 +47,8 @@ void GInterface::Init(HMODULE hModule)
 	AllocConsole();
 	freopen_s(&pFile, "CONOUT$", "w", stdout);
 
-	// Removed IncommingCrashWarning
-  // Removed AntiCrashHandler
+	SetUnhandledExceptionFilter(VectoredHandler);
+        // Removed AntiCrashHandler
 
 	thread(Monitor, hModule).detach();
 }
